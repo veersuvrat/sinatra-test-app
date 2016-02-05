ac96717be26975c6e28b2861728564a253fe0913
veersuvrat <veer2812@gmail.com>
2016-02-05 07:55:30 +0000

    Added stuff for nice.

--- app.rb
+++ app.rb
@@ -4,4 +4,7 @@ class MyApp < Sinatra::Base
   get '/' do
     "<!DOCTYPE html><html><head></head><body><h1>Hello World</h1></body></html>"
   end
+  
+  get '/nice' do
+    "<!DOCTYPE html><html><head></head><body><h2>This is pretty cool. From nice url.</h2></body></html>"
 end
